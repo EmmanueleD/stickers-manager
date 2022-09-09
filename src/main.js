549023bfdab990e0f30de86d8829076af94dba40
EmmanueleD <emmanuele.online@gmail.com>
2022-09-09 05:57:04 -0300

    add pinia, router, lottie, primevue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).mount('#app')
+import PrimeVueConfigurator from "./utils/PrimeVueConfigurator";
+
+import Vue3Lottie from "vue3-lottie";
+import "vue3-lottie/dist/style.css";
+
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+
+import "./assets/scss/main.scss";
+import "./registerServiceWorker";
+
+const pinia = createPinia();
+const app = createApp(App);
+
+PrimeVueConfigurator.init(app);
+
+app.use(pinia);
+app.use(router);
+app.use(Vue3Lottie);
+app.mount("#app");
