33ad5130b5cefe606c3dd6181e3bf23251440112
EmmanueleD <emmanuele.online@gmail.com>
2022-09-08 22:33:41 -0300

    add firma

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <h1>Stickers Manager</h1>
+
+  <div class="firma">
+    <a href="https://www.emmd.dev"><img src="./assets/emmd_logo.gif" alt=""></a>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
@@ -22,5 +26,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.firma {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  height: calc(100vh - 350px);
+}
+
+
+.firma img {
+  width: 75px
 }
 </style>
