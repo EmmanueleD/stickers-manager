c99b0431e29be1b1b8108156f5be177ab3fba813
EmmanueleD <emmanuele.online@gmail.com>
2022-09-11 17:45:21 -0300

    put stats page

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -1,17 +1,58 @@
 <template>
-  <div>
-
-    <PVCard id="piePerc" class="mb-4 pb-4">
+  <div class="fullpage">
+    <!-- <PVCard id="piePerc" class="mb-4 pb-4">
       <template #content>
         <PVChart type="pie" :data="pieData" :options="pieOptions"></PVChart>
       </template>
-    </PVCard>
+    </PVCard> -->
+    <div class="pie-title grid-center">
+      <h3>% COMPLETED</h3>
+    </div>
+    <div class="pie-container mb-4">
+      <PVChart type="pie" :data="pieData" :options="pieOptions"></PVChart>
+      <div class="perc-number">
+        <h4>26%</h4>
+      </div>
+    </div>
+    <div class="carousel-container">
 
+      <PVCarousel :value="carouselData" :numVisible="1" :numScroll="1" :responsiveOptions="responsiveOptions">
+        <template #item="data">
+          <h3 style="text-align: center">{{data.data.name}}</h3>
+          <div class="ranking-list">
 
-    <PVChart class="mt-4 pt-4" type="pie" :data="pieData" :options="pieWhiteOptions"></PVChart>
+            <div v-if="data.data.name == 'TEAMS'">
+              <div class="grid mb-4" v-for="item in data.data.ranking" :key="item.name">
 
+                <div class="col-3">
+                  <img style="width: 50px" src="../assets/images/flags/italy.png" alt="">
+                </div>
+                <div class="col-4 grid-center">
+                  {{ item.name }}
+                </div>
+                <div class="col-5 pt-4">
+                  <PVProgressBar :value="item.perc" :showValue="false"></PVProgressBar>
+                </div>
+              </div>
+            </div>
+            <div v-if="data.data.name == 'STICKERS'">
+              <div class="grid mb-4" v-for="(item, index) in data.data.ranking" :key="item.name">
 
-
+                <div class="col-1 pt-4">
+                  <h3>{{ index }}</h3>
+                </div>
+                <div class="col-6 grid-center">
+                  {{ item.name }}
+                </div>
+                <div class="col-5 pt-4">
+                  <PVProgressBar :value="item.perc" :showValue="false"></PVProgressBar>
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
+      </PVCarousel>
+    </div>
   </div>
 </template>
 
@@ -38,35 +79,181 @@ const pieOptions = ref({
         color: '#FFFFFF'
       }
     },
-    title: {
-      display: true,
-      text: 'Perc Completed',
-      color: '#000000',
-      font: {
-        family: '"Homenaje", sans-serif',
-        size: 35
-      }
-    }
   }
 });
 
-const pieWhiteOptions = ref({
-  plugins: {
-    legend: {
-      display: false,
-      labels: {
-        color: '#FFFFFF'
-      }
+
+const carouselData = ref(
+  [
+    {
+      name: 'TEAMS',
+      ranking: [
+        {
+          name: 'team 1',
+          perc: 30
+        },
+        {
+          name: 'team 2',
+          perc: 20
+        },
+        {
+          name: 'team 3',
+          perc: 5
+        },
+        {
+          name: 'team 4',
+          perc: 1
+        },
+        {
+          name: 'team 1',
+          perc: 30
+        },
+        {
+          name: 'team 2',
+          perc: 20
+        },
+        {
+          name: 'team 3',
+          perc: 5
+        },
+        {
+          name: 'team 4',
+          perc: 1
+        },
+        {
+          name: 'team 1',
+          perc: 30
+        },
+        {
+          name: 'team 2',
+          perc: 20
+        },
+        {
+          name: 'team 3',
+          perc: 5
+        },
+        {
+          name: 'team 4',
+          perc: 1
+        },
+      ]
     },
-    title: {
-      display: true,
-      text: 'Perc Completed',
-      color: '#ffffff',
-      font: {
-        family: '"Homenaje", sans-serif',
-        size: 35
-      }
+    {
+      name: 'STICKERS',
+      ranking: [
+        {
+          name: 'sticker 1',
+          perc: 13
+        },
+        {
+          name: 'sticker 2',
+          perc: 10
+        },
+        {
+          name: 'stiker 3',
+          perc: 5
+        },
+        {
+          name: 'sticker 4',
+          perc: 4
+        },
+        {
+          name: 'sticker 1',
+          perc: 13
+        },
+        {
+          name: 'sticker 2',
+          perc: 10
+        },
+        {
+          name: 'stiker 3',
+          perc: 5
+        },
+        {
+          name: 'sticker 4',
+          perc: 4
+        },
+        {
+          name: 'sticker 1',
+          perc: 13
+        },
+        {
+          name: 'sticker 2',
+          perc: 10
+        },
+        {
+          name: 'stiker 3',
+          perc: 5
+        },
+        {
+          name: 'sticker 4',
+          perc: 4
+        },
+        {
+          name: 'sticker 1',
+          perc: 13
+        },
+        {
+          name: 'sticker 2',
+          perc: 10
+        },
+        {
+          name: 'stiker 3',
+          perc: 5
+        },
+        {
+          name: 'sticker 4',
+          perc: 4
+        },
+        {
+          name: 'sticker 1',
+          perc: 13
+        },
+        {
+          name: 'sticker 2',
+          perc: 10
+        },
+        {
+          name: 'stiker 3',
+          perc: 5
+        },
+        {
+          name: 'sticker 4',
+          perc: 4
+        },
+        {
+          name: 'sticker 1',
+          perc: 13
+        },
+        {
+          name: 'sticker 2',
+          perc: 10
+        },
+        {
+          name: 'stiker 3',
+          perc: 5
+        },
+        {
+          name: 'sticker 4',
+          perc: 4
+        },
+      ]
     }
+  ]
+)
+
+const responsiveOptions = [
+
+  {
+    breakpoint: '3500px',
+    numVisible: 2,
+    numScroll: 2
+  },
+  {
+    breakpoint: '700px',
+    numVisible: 1,
+    numScroll: 1
   }
-});
+]
+
 </script>
--- src/views/layout/HeaderLayout.vue
+++ src/views/layout/HeaderLayout.vue
@@ -11,37 +11,24 @@
         <img src="../../assets/logo.png" style="width:100%">
       </div> -->
         <div class="col-5 grid-center">
-          <span class="router-link" :class="teamsActive? 'active': null" @click="goToTeams">TEAMS</span>
+          <span class="router-link" :class="router.currentRoute.value.path == '/'? 'active': null"
+            @click="$router.push('/')">TEAMS</span>
         </div>
         <div class="col-5 grid-center">
-          <span class="router-link" :class="statsActive? 'active': null" @click="goToStats">STATS</span>
+          <span class="router-link" :class="router.currentRoute.value.path == '/statistics'? 'active': null"
+            @click="$router.push('/statistics')">STATS</span>
         </div>
         <div class=" router-link col-2 grid-center">
           <i @click="authStore.logoutUser" class="pi pi-fw pi-sign-out"></i>
         </div>
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth';
 const authStore = useAuthStore()
 const router = useRouter()
-const teamsActive = ref(true)
-const statsActive = ref(false)
-function goToTeams() {
-  router.push('/')
-  teamsActive.value = true
-  statsActive.value = false
-}
-function goToStats() {
-  router.push('/statistics')
-  teamsActive.value = false
-  statsActive.value = true
-}
 </script> 
